6382b7c7fa7d9f0085c82243e3f8fd1337572b8b
Morteza3123 <mkhedry@gmail.com>
2021-11-07 01:58:15 +0330

    set stake function

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -10,9 +10,28 @@ contract TokenFarm {
     DappToken public dappToken;
     DaiToken public daiToken;
 
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance; 
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
+
     constructor (DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
+    }
+
+    function stakeTokens(uint _amount) public {
+
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
     }
     
 }
--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -8,11 +8,11 @@ require('chai')
 
 contract('TokenFarm', (accounts) => {
 
-    describe('Mock Dai deployment', async () => {
+    describe('Mock DAI deployment', async () => {
         it('has a name', async () => {
             let daiToken = await DaiToken.new()
             const name = daiToken.name()
-            assert.equal(name, 'Mock Dai Token')
+            assert.equal(name, 'Mock DAI Token')
         })
     })
 
