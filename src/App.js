80f51e7f79e8eb6ce4ff775aa5558edafca2d780
Morteza3123 <mkhedry@gmail.com>
2021-11-08 01:52:05 +0330

    add web3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import './App.css';
 import Navbar from './Components/Navbar';
 
 class App extends Component{
+
+  async componentWillMount(){
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData(){
+    const web3 = window.web3;
+
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account : accounts[0]})
+  }
+
+  //load web3
+  async loadWeb3(){
+    if (window.ethereum) {
+       window.web3 = new Web3(window.ethereum);
+       await window.ethereum.enable();
+     } else if (window.web3) {
+       window.web3 = new Web3(window.web3.currentProvider);
+     } else {
+       window.alert(
+         "Non-Ethereum browser detected. You should consider trying MetaMask!"
+       );
+     }
+ }
+
   
   constructor(props){
     super(props)
     this.state = {
-      account : '0x0'
+      account : '',
+      daiToken : {},
+      dappToken : {},
+      tokenFarm : {},
+      daiTokenBalance: '0',
+      dappTokenBalance: '0',
+      stakingBalance: '0',
+      loading : true
     }
   }
 
   render(){
     return (
-      <div>
+      <div  className=''>
         <Navbar account={this.state.account} />
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 ml-auto mr outo" >
+        <div className="container fluid mt-5  ">
+          <div className="row justify-content-center">
+            <main role="main" className="col-lg-12 ml-auto mr outo" style={{  maxWidth: '600px'}}>
               <div className="content mr-auto ml-auto">
                 <a 
                   href="http://localhost:3000/"
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,13 +15,13 @@ class Navbar extends Component {
                     &nbsp; Dapp Token  Farm
                 </a>
 
-                <ui  className="navbar-nav px-3">
-                    <li className="nav-item text-nowrap d-none  d-sm-none   d-sm-block">
-                        <small  className="text-secondary">
-                            <small id="account">{this.props.account}</small>
-                        </small>
-                    </li>
-                </ui>
+                <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-sm-block ">
+              <small className="text-secondary">
+                  <small id="account">{this.props.account}</small>
+              </small>
+              </li>
+              </ul>
             </nav>
         )
     }
